f6d30e061e83e11fef03ded8ba5f2bf05ca033c2
Prajwala Immareddy <pimmareddy5@gmail.com>
2025-11-07 23:52:54 -0500

    added linked button

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,23 +3,21 @@ import React from 'react';
 function Contact() {
   return (
     <div className="contact-page">
-      <h1 style={{ color: '#00ff00', fontSize: '3rem', marginBottom: '2rem' }}>CONTACT US</h1>
       
       {/* arcade image */}
-      <div style={{ maxWidth: '400px', margin: '0 auto' }}>
-        <img 
+      <div style={{ maxWidth: '400px', margin: '0 auto'}}>
+        <img
           src="/arcade-contact.png" 
           alt="Arcade Contact" 
           style={{ 
-            width: '100%', 
-            height: 'auto',
-            display: 'block',
-            borderRadius: '15px'
+            width: '150%', 
+            height: '200%'
           }}
         />
+        < a href="mailto:pimmareddy5@gmail.com" class="overlay-button">Click Here to Contact Us</a>
       </div>
     </div>
-  );
+  )
 }
 
 export default Contact;
