d6b6e8f1a63aec7b68da0305df16d75d1cf340a0
natasha <joshi.nata@northeastern.edu>
2025-11-13 20:35:32 -0500

    added fun facts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,45 +33,46 @@ function Home() {
             backgroundColor: 'rgba(0, 0, 0, 0)'
           }}></div>
         </div>
-        {/* Text below image */}
-        <h1 style={{ 
-          color: '#ffffffb6', 
-          position: 'absolute',
-          fontSize: '1.5rem', 
-          top: '350px',
-          right: '150px',
-          // marginBottom: '2rem', 
-          // textAlign: 'right',
-          fontFamily: 'sans-serif',
-        }}>
-          Unsure what to do on your night out?<br />
-          Tired of doing the same activity?<br />
-          Eating at the same place?
-        </h1>
-        {/* Secondary image on top of the background
-        <img
-          src="/logo.png"
-          alt="logo"
+       {/* Text instead of logo */}
+        <div
           style={{
             position: 'absolute',
-            top: '50%',
-            left: '80%',
-            transform: 'translate(-50%, -50%)',
-            width: '200px',
-            height: '200px',
-            objectFit: 'contain'
+            bottom: '450px', // adjust distance from bottom of header
+            left: '0%',
+            transform: 'translateX(35%)',
+            color: 'rgba(255, 255, 255, 0.9)',
+            fontSize: '1.8rem',
+            fontWeight: 'bold',
+            textAlign: 'center',
+            fontFamily: 'Aldrich'
           }}
-        ></img> */}
+        >
+          From Cravings to Quests - Insert Choice and Let The Game Begin
+
+        </div>
         <img
           src="/instructions.png"
           alt="instructions"
           style={{
             position: 'absolute',
-            top: '80%',
+            top: '82%',
             left: '30%',
             transform: 'translate(-50%, -50%)',
             width: '800px',
             height: '800px',
+            objectFit: 'contain'
+          }}
+        ></img>
+                <img
+          src="/funfacts.png"
+          alt="FunFacts"
+          style={{
+            position: 'absolute',
+            top: '82%',
+            left: '78%',
+            transform: 'translate(-50%, -50%)',
+            width: '550px',
+            height: '550px',
             objectFit: 'contain'
           }}
         ></img>
